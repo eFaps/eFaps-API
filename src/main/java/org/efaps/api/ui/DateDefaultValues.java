a2a40d6b49772c76f68717303b2472a41635a343
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2015-02-16 20:00:01 +0000

    - api: adding date default values
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-API/trunk@14853 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/java/org/efaps/api/ui/DateDefaultValues.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2003 - 2015 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.api.ui;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id: $
+ */
+public enum DateDefaultValues
+{
+    /** The Value is today. */
+    TODAY,
+    /** The Value represents today +-n weeks. */
+    WEEKS,
+    /** The Value represents today +-n month. */
+    MONTHS,
+    /** The Value represents today +-n years. */
+    YEARS,
+    /**
+     * The Value represents today 1,2,3,4,5,6,7. the Integer represents on of
+     * the weekdays starting with Monday, Tuesday...
+     */
+    WITHDAYOFWEEK,
+    /** Integer day of month. */
+    WITHDAYOFMONTH,
+    /** like today will be evaluated.*/
+    LASTDAYOFMONTH;
+}
