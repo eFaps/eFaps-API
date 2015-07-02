7053b0227394dc87d929bb6639687eb73dd95f4b
Jan Moxter <jan.moxter@innobix.com>
2015-07-02 01:00:48 -0500

    - adding annotation for SytemConfiguration

---
+++ src/main/java/org/efaps/api/annotation/EFapsSystemConfiguration.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2003 - 2015 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.efaps.api.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to mark a class that it contains SystemConfigurations.
+ *
+ * @author The eFaps Team
+ */
+@Documented
+@Retention(RUNTIME)
+@Target(TYPE)
+@Inherited
+public @interface EFapsSystemConfiguration
+{
+
+    /**
+     * UUID of the SustemConfiguration the class belongs to.
+     */
+    String value();
+}
