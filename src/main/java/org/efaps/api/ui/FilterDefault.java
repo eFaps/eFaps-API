eb34edc7e1e925a0b2d96a0d566d1e8a1e347e57
Jan Moxter <jan.moxter@innobix.com>
2016-12-16 00:59:13 -0500

    - Issue eFaps/eFaps-WebApp#116: Use interfaces to pass the filter to esjp

---
+++ src/main/java/org/efaps/api/ui/FilterDefault.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2003 - 2016 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+package org.efaps.api.ui;
+
+
+/**
+ * The Enum FilterDefault.
+ *
+ * @author The eFaps Team
+ */
+public enum FilterDefault
+{
+    /** the filter default is for today. */
+    TODAY,
+    /** the filter default is for the last week. */
+    WEEK,
+    /** the filter default is for the last month. */
+    MONTH,
+    /** the filter default is for the last year. */
+    YEAR,
+    /** the filter default is for all the existing elements. */
+    ALL,
+    /** the filter default is for nothing of the existing elements. */
+    NONE;
+}
